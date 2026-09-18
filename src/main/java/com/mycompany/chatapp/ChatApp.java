f68fd4712890a028420d84db1deb71b0d12eb729
Student <Student@LabVM2049939.vlmp1z3n5ohufhgbtk5spuu32h.jnbx.internal.cloudapp.net>
2026-09-18 19:01:37 +0200

    created a display that is user friendly so that the user can register and login without any complications

--- src/main/java/com/mycompany/chatapp/ChatApp.java
+++ src/main/java/com/mycompany/chatapp/ChatApp.java
@@ -11,5 +11,38 @@ package com.mycompany.chatapp;
 public class ChatApp {
 
     public static void main(String[] args) {
+
+
+        UserRegestration user = new UserRegestration();
+
+        System.out.println("===== USER REGISTRATION =====");
+
+        // Register username
+        boolean usernameRegistered = user.Username();
+
+        // Register password
+        boolean passwordRegistered = user.Password();
+
+        // Register cellphone number
+        user.Cellphone();
+
+        // Check whether registration details are valid
+        if (usernameRegistered && passwordRegistered) {
+
+            System.out.println("\n===== LOGIN =====");
+
+            // Attempt to login
+            boolean loginSuccessful = user.Login();
+
+            if (loginSuccessful) {
+                System.out.println("Login successful!");
+            } else {
+                System.out.println("Login failed.");
+            }
+
+        } else {
+            System.out.println("\nRegistration was unsuccessful.");
+            System.out.println("Please correct your username and password.");
+        }
     }
 }
